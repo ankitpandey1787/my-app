7f1db9e14636efdc97b6dc0e6c8d7e4feb7a0085
ankitpandey1787 <ankit.pandey_cs18@gla.ac.in>
2021-03-05 18:42:16 +0530

    Files added for repo

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My first Code
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'violet',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
