342f2c9a8ab6d433a6a8331ba240078645bd0072
ankitpandey1787 <ankit.pandey_cs18@gla.ac.in>
2021-03-05 18:54:22 +0530

    new code

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MY first react native code</Text>
+      <Text>MY first react native</Text>
       <StatusBar style="auto" />
     </View>
   );
